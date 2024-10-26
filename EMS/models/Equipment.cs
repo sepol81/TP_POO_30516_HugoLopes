85d1089ab4e8ec5cbac784097316803ad9869ddd
a30516 <151578391+sepol81@users.noreply.github.com>
2024-10-26 18:28:03 +0100

    up

--- EMS/models/Equipment.cs
+++ EMS/models/Equipment.cs
@@ -1,14 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace EMS.models
 {
-    #region Propreties
+    #region Propreties    
+    /// <summary>
+    /// Represents an equipment item used in rescue activities. 
+    /// </summary>
     public class Equipment
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
@@ -16,11 +21,19 @@ namespace EMS.models
         public DateTime MaintenanceDate { get;set;}
         public string Status { get; set; }
 
-               
-    
-    #endregion
-    #region Constructors
 
+
+        #endregion
+        #region Constructors        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Equipment"/> class.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="name">The name.</param>
+        /// <param name="description">The description.</param>
+        /// <param name="quantity">The quantity.</param>
+        /// <param name="maintenanceDate">The maintenance date.</param>
+        /// <param name="status">The status.</param>
         public Equipment (int id, string name, string description, int quantity, DateTime maintenanceDate, string status) 
         {
             Id = id;
